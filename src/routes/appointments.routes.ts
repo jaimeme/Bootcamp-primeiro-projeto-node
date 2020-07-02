38a7fe57295b639c142f0224a09de34316e29c28
Jaime Rieiro <jaimeribeiro99@gmail.com>
2020-07-02 01:16:20 -0300

    :art: Adding diretory:"repositories"

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,35 +1,29 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRespository from '../repositories/AppointmentsRespository';
 
 const appointmentsRouter = Router();
-interface Appointment {
-    id: string;
-    provider: string;
-    date: Date;
-}
-const appointments: Appointment[] = [];
+const appointmentsRespository = new AppointmentsRespository();
 
 appointmentsRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
 
     const parsedDate = startOfHour(parseISO(date));
-    const findAppointmentInSameDate = appointments.find(appointment =>
-        isEqual(parsedDate, appointment.date),
+    const findAppointmentInSameDate = appointmentsRespository.findByDate(
+        parsedDate,
     );
-
+    // se tiver um valor dentro do findAppointmentInSameDate ele vai entrar no if e retornar a mensagem, caso não tenha valor ele vai retornar undefined
     if (findAppointmentInSameDate) {
         return response
             .status(400)
             .json({ message: 'This appointment is already booked' });
     }
-    const appointment = {
-        id: uuid(),
-        provider,
-        date: parsedDate,
-    };
-    appointments.push(appointment);
+    const appointment = appointmentsRespository.create(provider, parsedDate);
+
     return response.json(appointment);
 });
 
 export default appointmentsRouter;
+
+// parte 4
