4bf9ab0e64f031320a450a07592bbaed830188f5
Jaime Rieiro <jaimeribeiro99@gmail.com>
2020-07-14 13:34:41 -0300

    ✨Create User

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
@@ -15,14 +16,15 @@ appointmentsRouter.get('/', async (request, response) => {
 });
 appointmentsRouter.post('/', async (request, response) => {
     try {
-        const { provider, date } = request.body;
+        const { provider_id, date } = request.body;
 
         const parsedDate = parseISO(date);
+
         const createAppointment = new CreateAppointmentService();
 
         const appointment = await createAppointment.execute({
             date: parsedDate,
-            provider,
+            provider_id,
         });
         return response.json(appointment);
     } catch (err) {
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
@@ -5,12 +6,15 @@ import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface RequestDTO {
-    provider: string;
+    provider_id: string;
     date: Date;
 }
 
 class CreateAppointmentService {
-    public async execute({ date, provider }: RequestDTO): Promise<Appointment> {
+    public async execute({
+        date,
+        provider_id,
+    }: RequestDTO): Promise<Appointment> {
         const appointmentsRepository = getCustomRepository(
             AppointmentsRepository,
         );
@@ -22,7 +26,7 @@ class CreateAppointmentService {
             throw Error('this appointment is already booked');
         }
         const appointment = appointmentsRepository.create({
-            provider,
+            provider_id,
             date: appointmentDate,
         });
 
