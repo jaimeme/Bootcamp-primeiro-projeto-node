64d753ff7a1c2b4384bbe90920620c240b1756a2
Jaime Rieiro <jaimeribeiro99@gmail.com>
2020-07-15 17:53:18 -0300

    :sparkles: Token Generation for Session

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface requestDTO {
 
 interface responseDTO {
     user: User;
+    token: string;
 }
 
 class AuthenticateUserService {
@@ -28,7 +30,12 @@ class AuthenticateUserService {
             throw new Error('Incorrect email/password combination');
         }
 
-        return { user };
+        const token = sign({}, '71ce3d0643c01de3024af66c79470695', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
+        return { user, token };
     }
 }
 
